39076fa2ce041b5a13c91b08f8ce8e0b50233b4a
Sebastian velez <sebasvelez714@gmail.com>
2023-08-26 10:19:41 -0500

    se crea la estrutura inicial

---
+++ Js/main.js
@@ -0,0 +1,12 @@
+const menulogin =document.querySelector('.navbar-login');
+menulogin.addEventListener('click', AbrirLogin);
+
+
+
+
+function AbrirLogin(){
+    console.log('clik icono login');
+
+    
+
+}
