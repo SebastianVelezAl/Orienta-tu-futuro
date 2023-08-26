7f94d8658810115ebfa0d38debb6bf4a8d835689
Sebastian velez <sebasvelez714@gmail.com>
2023-08-26 13:31:55 -0500

    avance login

--- Js/main.js
+++ Js/main.js
@@ -1,4 +1,7 @@
 const menulogin =document.querySelector('.navbar-login');
+const login =document.querySelector('.login');
+const frmlogin =document.querySelector('.form-container-login');
+
 menulogin.addEventListener('click', AbrirLogin);
 
 
@@ -7,6 +10,8 @@ menulogin.addEventListener('click', AbrirLogin);
 function AbrirLogin(){
     console.log('clik icono login');
 
+        console.log('no entra al if');
+        login.classList.toggle('inactive')
+        frmlogin.classList.toggle('inactive');
     
-
 }
