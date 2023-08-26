5ae1c16e583367503f82990895a7b6990bd0465d
Sebastian velez <sebasvelez714@gmail.com>
2023-08-26 17:28:20 -0500

    Se inicia proceso del banner inicial (buscar)

--- Js/main.js
+++ Js/main.js
@@ -1,5 +1,4 @@
 const menulogin =document.querySelector('.navbar-login');
-const login =document.querySelector('.login');
 const frmlogin =document.querySelector('.form-container-login');
 
 menulogin.addEventListener('click', AbrirLogin);
@@ -10,8 +9,7 @@ menulogin.addEventListener('click', AbrirLogin);
 function AbrirLogin(){
     console.log('clik icono login');
 
-        console.log('no entra al if');
-        login.classList.toggle('inactive')
-        frmlogin.classList.toggle('inactive');
+    console.log('no entra al if');
+    frmlogin.classList.toggle('inactive');
     
 }
